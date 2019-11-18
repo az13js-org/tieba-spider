49c6b72203335530b65cb05811f50f57763a4fb5
az13js <1654602334@qq.com>
2019-11-18 22:41:27 +0800

    增加一个模块：最新帖子，增加随机暂停5分钟后爬数据

--- test.php
+++ test.php
@@ -30,6 +30,20 @@ function saveOrUpdateUserData(Connect $c, string $text, bool $isNew)
     }
 }
 
+function saveTiezi(Connect $c, string $tiezi, string $tieba)
+{
+    $content = addslashes($tiezi);
+    $name = addslashes($tieba);
+    $c->send("SELECT `id` FROM `posts` WHERE `tieba_name`=\"$name\" AND `post`=\"$content\"");
+    $exis = $c->recv();
+    $now = date('Y-m-d H:i:s');
+    if (empty($exis)) {
+        $c->send("INSERT INTO `posts`(`create_time`,`update_time`,`tieba_name`,`post`) VALUES (\"$now\",\"$now\",\"$name\",\"$content\")");
+    } else {
+        $c->send("UPDATE `posts` SET `version`=`version`+1,`update_time`=\"$now\" WHERE `id`={$exis[0]['id']}");
+    }
+}
+
 function compareForLastTiebaHotDataAndFindNew(string $tiebaId, array $hot): array
 {
     if (empty($hot)) {
@@ -90,13 +104,16 @@ $x->send(file_get_contents(__DIR__ . DIRECTORY_SEPARATOR . 'database.sql'));
 $cookie = '';
 $name = $config['tieba'];
 $s = new Tieba($name, $cookie);
-
 file_put_contents(__DIR__ . DIRECTORY_SEPARATOR . 'log' . DIRECTORY_SEPARATOR . 'test.log', date('Y-m-d H:i:s') . ' 解析数据' . PHP_EOL, FILE_APPEND);
+$posts = $s->getAllText();
 foreach ($s->getTiebaHot() as $k => $v_arr) {
     /* $k 对应第几个贴吧，$v_arr 贴吧的热铁数组 */
     $newHotPosting = compareForLastTiebaHotDataAndFindNew($k, $v_arr);
     foreach ($v_arr as $kk => $v) {
         saveOrUpdateTiebaHotData($x, $name[$k], $v, in_array($v, $newHotPosting));
+    }
+    foreach ($posts[$k] as $tiezi) {
+        saveTiezi($x, $tiezi, $name[$k]);
     }
 }
 
@@ -105,3 +122,7 @@ $newAuthors = compareForLastAuthorsAndFindNew($allAuthors);
 foreach ($allAuthors as $k => $v) {
     saveOrUpdateUserData($x, $v, in_array($v, $newAuthors));
 }
+
+$idle = mt_rand(1, 5 * 60);
+file_put_contents(__DIR__ . DIRECTORY_SEPARATOR . 'log' . DIRECTORY_SEPARATOR . 'test.log', date('Y-m-d H:i:s') . ' 暂停 ' . $idle . ' 秒' . PHP_EOL, FILE_APPEND);
+sleep($idle);
